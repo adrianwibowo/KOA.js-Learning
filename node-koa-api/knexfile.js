0d6ce1f018d11bd4b0f8c2278d9b16f95cd7186d
adrian <elaawers@gmail.com>
2021-06-22 11:56:53 +0700

    [law] CRUD KOA with knex Mocha Chai Postgres

---
+++ node-koa-api/knexfile.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
+
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: {
+      database: 'koa_learn',
+      user:     'passfox',
+      password: 'fox'
+    },
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'koa_learn',
+      user:     'passfox',
+      password: 'fox'
+    },
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  }
+};
